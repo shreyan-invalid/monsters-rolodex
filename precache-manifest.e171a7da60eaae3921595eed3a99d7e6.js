2c60dcf6932bb52ab8412b2904771e692026a1aa
Shreyan <shreyan.ghosh9@gmail.com>
2020-10-27 22:48:34 +0530

    Updates

---
+++ precache-manifest.e171a7da60eaae3921595eed3a99d7e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa1732c95b62dac6d32ea3767b278e0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "641ba53b47b28f1daf57",
+    "url": "/monsters-rolodex/static/css/main.14b45044.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641ba53b47b28f1daf57",
+    "url": "/monsters-rolodex/static/js/main.6daf1287.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
